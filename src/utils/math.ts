84c2848e79eeab3702d83b8e21795e92f3d70f9a
dawsin <dawsinb@gmail.com>
2022-01-17 09:11:00 -0500

    update util documentation

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -1,14 +1,19 @@
 /**
+ * @module Utils
+ * @mergeTarget
+ */
+
+/**
  * Linearlly interpolates between the given values.
  *
  * @param start the number to start from
  * @param end the number to progress to
- * @param progress the amount progressed as a number in the range [0, 1]
+ * @param progress the amount progressed as a number in the range `[0, 1]`
  * @returns
  * the result of a linear interpolation between `start` and `end` at the given `progress` percent.
  *
  * *To prevent an infinite loop in a recursive lerp if `start` is within 0.0001 of `end` it will return `end`*
- * @category Util
+ * @category Math
  */
 function lerp(start: number, end: number, progress: number) {
   // jump to end if start is very close to prevent an inifinite loop in a recursive lerp
@@ -28,7 +33,7 @@ function lerp(start: number, end: number, progress: number) {
  * @param min the minimum value of the range
  * @param max the maximum value of the range
  * @returns `value` if it is within the range `[min, max]`;  `min` if `value < min`;  `max` if `value > max`
- * @category Util
+ * @category Math
  */
 function clamp(value: number, min: number, max: number) {
   return Math.min(Math.max(value, min), max);
@@ -40,7 +45,7 @@ function clamp(value: number, min: number, max: number) {
  * @param min the minimum value of the range
  * @param progress the maximum value of the range
  * @returns a random value within the range `[min, max)`
- * @category Util
+ * @category Math
  */
 function randomRange(min: number, max: number) {
   return Math.random() * (max - min) + min;
--- src/utils/shaders.ts
+++ src/utils/shaders.ts
@@ -1,4 +1,12 @@
-/** Template string tag to enable code highlighting in glsl strings */
+/**
+ * @module Utils
+ * @mergeTarget
+ */
+
+/** 
+ * Template string tag to enable code highlighting in glsl strings 
+ * @category Shader
+ */
 const glsl = (x: TemplateStringsArray) => x.toString();
 
 export { glsl };
