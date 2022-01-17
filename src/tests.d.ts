1621d6699e6195318c08910fe79efec489c147c6
dawsin <dawsinb@gmail.com>
2022-01-17 11:04:15 -0500

    delete unused type declaration file

--- src/tests.d.ts
+++
@@ -1,4 +0,0 @@
-// these type declarations are only used for testing, they are not accessible in source code
-
-// override read only properties to be manually editable for testing
-type MutableReadonly<T> = { -readonly [P in keyof T]-?: T[P] };
