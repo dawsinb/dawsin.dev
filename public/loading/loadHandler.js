88a79d70ba058af92d152a2b42e8b6f4edebafed
dawsin <dawsinb@gmail.com>
2022-01-19 14:30:42 -0500

    remove console logs

--- public/loading/loadHandler.js
+++ public/loading/loadHandler.js
@@ -7,7 +7,6 @@ window.addEventListener('load', () => {
 const loadedAssets = [];
 window.addEventListener('assetLoad', (event) => {
   loadedAssets.push(event.detail)
-  console.log(event.detail)
 });
 
 // load asset map
@@ -19,7 +18,7 @@ fetch('/loading/assetMap.json')
     for (const asset in assetMap) {
       totalBytes += assetMap[asset];
     }
-    console.log(assetMap)
+    
     const processAssets = () => {
       // calculate how many bytes have been loaded
       let loadedBytes = 0;
