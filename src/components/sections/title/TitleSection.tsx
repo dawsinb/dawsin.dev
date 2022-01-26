43a8f481c9c9805348a859079ce21b2871b53867
dawsin <dawsinb@gmail.com>
2022-01-25 19:31:16 -0500

    update background parallax

--- src/components/sections/title/TitleSection.tsx
+++ src/components/sections/title/TitleSection.tsx
@@ -135,15 +135,15 @@ function TitleSection({ index, parallax }: TitleProps) {
         </group>
       </SectionItem>
 
-      <SectionItem parallax={4}>
+      <SectionItem parallax={20}>
+        <Refractor index={index} />
+      </SectionItem>
+
+      <SectionItem parallax={2}>
         <mesh rotation={[0, 0, Math.PI / 8]} position={[0, 0, -10]}>
           <planeBufferGeometry args={[size.width * 3, size.height * 2, 32, 32]} />
           <meshBasicMaterial color={'#000'} />
         </mesh>
-      </SectionItem>
-
-      <SectionItem parallax={20}>
-        <Refractor index={index} />
       </SectionItem>
 
       <SectionItem parallax={4}>
