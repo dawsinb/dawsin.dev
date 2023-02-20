a0d131d053a715a1ea3479450338578daf51fcf9
Dawsin Blanchard <dawsin@yaraku.com>
2023-02-21 06:36:19 +0900

    formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { MutableRefObject, Suspense, useEffect, useRef, useState } from 'react';
 import { createRoot, events, ReconcilerRoot } from '@react-three/fiber';
-import { PerformanceMonitor, Stats } from "@react-three/drei";
+import { PerformanceMonitor, Stats } from '@react-three/drei';
 import styled from 'styled-components';
 import { useLayout } from 'stores/layout';
 import { useLanguage } from 'stores/language';
@@ -87,7 +87,7 @@ function App() {
       if (!canvasRootRef.current) {
         canvasRootRef.current = createRoot(canvasHTMLRootRef.current);
       }
-      
+
       canvasRootRef.current.configure({
         orthographic: true,
         camera: { position: [0, 0, 10000], far: 20000 },
@@ -97,14 +97,16 @@ function App() {
         flat: true,
         legacy: true,
         events
-      })
-      
-      canvasRootRef.current.render(  
-        <PerformanceMonitor 
+      });
+
+      canvasRootRef.current.render(
+        <PerformanceMonitor
           bounds={(deviceFrameRate) => [50, Math.min(deviceFrameRate, 144)]}
-          factor={1} 
-          onChange={({ factor }) => {setDpr(Math.round((0.5 + 1.5 * factor) * 10) / 10)}}
-        >       
+          factor={1}
+          onChange={({ factor }) => {
+            setDpr(Math.round((0.5 + 1.5 * factor) * 10) / 10);
+          }}
+        >
           <Stats showPanel={0} />
           <group>
             <TitleSection index={0} parallax={1} />
@@ -117,10 +119,10 @@ function App() {
             <EndSection index={7} parallax={1} />
             <Background numSections={sectionNames.length} />
           </group>
-        </PerformanceMonitor> 
+        </PerformanceMonitor>
       );
     }
-  }
+  };
 
   // rerender the application when the dpr changes
   useEffect(() => render(), [dpr]);
@@ -132,7 +134,7 @@ function App() {
       useLayout.setState({ isVertical: window.innerHeight > window.innerWidth });
 
       // rerender the app to reflect changes
-      render()
+      render();
     });
 
     // fire resize event for initial sizing
@@ -142,7 +144,7 @@ function App() {
   return (
     <>
       <AppContainer>
-        <Suspense fallback={null}>          
+        <Suspense fallback={null}>
           <CanvasRoot ref={canvasHTMLRootRef} />
         </Suspense>
       </AppContainer>
