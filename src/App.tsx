7731abd354b88e7742a72b71ec84e57339730bba
Dawsin Blanchard <dawsin@yaraku.com>
2023-02-21 06:35:44 +0900

    Dynamic Performance Scaling

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { Suspense, useEffect, useRef } from 'react';
-import { createRoot, events } from '@react-three/fiber';
+import { MutableRefObject, Suspense, useEffect, useRef, useState } from 'react';
+import { createRoot, events, ReconcilerRoot } from '@react-three/fiber';
+import { PerformanceMonitor, Stats } from "@react-three/drei";
 import styled from 'styled-components';
 import { useLayout } from 'stores/layout';
 import { useLanguage } from 'stores/language';
@@ -73,29 +74,38 @@ function App() {
   ];
 
   // create ref to root element of the canvas so we can create a R3F render tree from it
-  const canvasRootRef = useRef<HTMLCanvasElement>(null);
+  const canvasHTMLRootRef = useRef<HTMLCanvasElement>(null);
+  const canvasRootRef = useRef(null) as MutableRefObject<ReconcilerRoot<HTMLCanvasElement> | null>;
 
-  // add resize event listener
-  useEffect(() => {
-    window.addEventListener('resize', () => {
-      // determine if vertical layout
-      useLayout.setState({ isVertical: window.innerHeight > window.innerWidth });
+  // dynamically adjust DPR using performance monitor
+  const [dpr, setDpr] = useState(2);
 
-      // create r3f canvas
-      if (canvasRootRef.current) {
-        const root = createRoot(canvasRootRef.current);
-        root.configure({
-          orthographic: true,
-          camera: { position: [0, 0, 10000], far: 20000 },
-          gl: { antialias: true },
-          dpr: [1, 1],
-          linear: true,
-          flat: true,
-          legacy: true,
-          events
-        })
-        
-        root.render(
+  // renders the R3F canvas content
+  const render = () => {
+    // create r3f canvas
+    if (canvasHTMLRootRef.current) {
+      if (!canvasRootRef.current) {
+        canvasRootRef.current = createRoot(canvasHTMLRootRef.current);
+      }
+      
+      canvasRootRef.current.configure({
+        orthographic: true,
+        camera: { position: [0, 0, 10000], far: 20000 },
+        gl: { antialias: true },
+        dpr: dpr,
+        linear: true,
+        flat: true,
+        legacy: true,
+        events
+      })
+      
+      canvasRootRef.current.render(  
+        <PerformanceMonitor 
+          bounds={(deviceFrameRate) => [50, Math.min(deviceFrameRate, 144)]}
+          factor={1} 
+          onChange={({ factor }) => {setDpr(Math.round((0.5 + 1.5 * factor) * 10) / 10)}}
+        >       
+          <Stats showPanel={0} />
           <group>
             <TitleSection index={0} parallax={1} />
             <AboutSection index={1} parallax={1.5} />
@@ -107,8 +117,22 @@ function App() {
             <EndSection index={7} parallax={1} />
             <Background numSections={sectionNames.length} />
           </group>
-        );
-      }
+        </PerformanceMonitor> 
+      );
+    }
+  }
+
+  // rerender the application when the dpr changes
+  useEffect(() => render(), [dpr]);
+
+  // add resize event listener
+  useEffect(() => {
+    window.addEventListener('resize', () => {
+      // determine if vertical layout
+      useLayout.setState({ isVertical: window.innerHeight > window.innerWidth });
+
+      // rerender the app to reflect changes
+      render()
     });
 
     // fire resize event for initial sizing
@@ -118,8 +142,8 @@ function App() {
   return (
     <>
       <AppContainer>
-        <Suspense fallback={null}>
-          <CanvasRoot ref={canvasRootRef} />
+        <Suspense fallback={null}>          
+          <CanvasRoot ref={canvasHTMLRootRef} />
         </Suspense>
       </AppContainer>
 
