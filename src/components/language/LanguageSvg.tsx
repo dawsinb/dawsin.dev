876f48a64f7cedad0555c7f4937890f1ddc8d9b6
Dawsin Blanchard <dawsin@yaraku.com>
2023-04-06 19:43:54 +0900

    add animation

--- src/components/language/LanguageSvg.tsx
+++ src/components/language/LanguageSvg.tsx
@@ -3,6 +3,22 @@
  * @mergeTarget
  */
 
+import styled from 'styled-components';
+
+/** Container for {@link LanguageSvg} */
+const Container = styled('div')`
+  width: 100%;
+  cursor: pointer;
+  // expand slightly on hover
+  &:hover {
+    transform: scale(1.05);
+  }
+  // move down to simulate being pressed on click
+  &:active {
+    transform: translateY(2%);
+  }
+`;
+
 /** Props for {@link LanguageSvg} */
 interface LanguageSvgProps {
   /** Color of the svg */
@@ -19,29 +35,31 @@ interface LanguageSvgProps {
  */
 function LanguageSvg({ color, diameter = 100, strokeWidth = 2 }: LanguageSvgProps) {
   return (
-    <svg
-      viewBox={`
+    <Container>
+      <svg
+        viewBox={`
         ${-strokeWidth} 
         ${-strokeWidth} 
         ${diameter + strokeWidth * 2} 
         ${diameter + strokeWidth * 2}
       `}
-    >
-      <circle
-        r={diameter / 2}
-        cx={diameter / 2}
-        cy={diameter / 2}
-        strokeWidth={strokeWidth}
-        stroke={color}
-        fill="none"
-      />
-      <line x1="50" y1="0" x2="50" y2="100" stroke={color} strokeWidth={strokeWidth} />
-      <line x1="0" y1="50" x2="100" y2="50" stroke={color} strokeWidth={strokeWidth} />
-      <path d="M50,0 C50,0 -20,50 50,100" fill="none" stroke={color} strokeWidth={strokeWidth} />
-      <path d="M50,0 C50,0 120,50 50,100" fill="none" stroke={color} strokeWidth={strokeWidth} />
-      <path d="M10,20 C10,20 50,50 90,20" fill="none" stroke={color} strokeWidth={strokeWidth} />
-      <path d="M10,80 C10,80 50,50 90,80" fill="none" stroke={color} strokeWidth={strokeWidth} />
-    </svg>
+      >
+        <circle
+          r={diameter / 2}
+          cx={diameter / 2}
+          cy={diameter / 2}
+          strokeWidth={strokeWidth}
+          stroke={color}
+          fill="none"
+        />
+        <line x1="50" y1="0" x2="50" y2="100" stroke={color} strokeWidth={strokeWidth} />
+        <line x1="0" y1="50" x2="100" y2="50" stroke={color} strokeWidth={strokeWidth} />
+        <path d="M50,0 C50,0 -20,50 50,100" fill="none" stroke={color} strokeWidth={strokeWidth} />
+        <path d="M50,0 C50,0 120,50 50,100" fill="none" stroke={color} strokeWidth={strokeWidth} />
+        <path d="M10,20 C10,20 50,50 90,20" fill="none" stroke={color} strokeWidth={strokeWidth} />
+        <path d="M10,80 C10,80 50,50 90,80" fill="none" stroke={color} strokeWidth={strokeWidth} />
+      </svg>
+    </Container>
   );
 }
 
